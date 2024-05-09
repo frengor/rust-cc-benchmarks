1f06052093daf102fdf9d8b1d78366ed2a277107
fren_gor <goro@frengor.com>
2024-05-10 01:12:32 +0200

    Set measurement time to 10s

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,5 @@
-use criterion::{criterion_group, criterion_main};
+use std::time::Duration;
+use criterion::{Criterion, criterion_group, criterion_main};
 
 #[cfg(all(feature = "jemalloc", not(target_env = "msvc")))]
 use tikv_jemallocator::Jemalloc;
@@ -12,10 +13,13 @@ mod binary_trees;
 mod binary_trees_with_parent_pointers;
 mod large_linked_list;
 
-criterion_group!(benches,
-    stress_test::stress_test,
-    binary_trees::binary_trees,
-    binary_trees_with_parent_pointers::binary_trees_with_parent_pointers,
-    large_linked_list::large_linked_list
+criterion_group!(
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(10));
+    targets =
+        stress_test::stress_test,
+        binary_trees::binary_trees,
+        binary_trees_with_parent_pointers::binary_trees_with_parent_pointers,
+        large_linked_list::large_linked_list
 );
 criterion_main!(benches);
