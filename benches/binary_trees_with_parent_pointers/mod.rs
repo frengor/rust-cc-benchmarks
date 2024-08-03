f63a5c349690f46135aec7839cf84a853a4ab02f
fren_gor <goro@frengor.com>
2024-08-03 04:07:31 +0200

    Make every crate optional

--- benches/binary_trees_with_parent_pointers/mod.rs
+++ benches/binary_trees_with_parent_pointers/mod.rs
@@ -1,9 +1,14 @@
 use criterion::Criterion;
 
+#[cfg(feature = "rc")]
 mod rc;
+#[cfg(feature = "arc")]
 mod arc;
+#[cfg(feature = "rust-cc")]
 mod rust_cc;
+#[cfg(feature = "gc")]
 mod gc;
+#[cfg(feature = "safe-gc")]
 mod safe_gc;
 #[cfg(feature = "zb-safe-gc")]
 mod zb_safe_gc;
@@ -13,15 +18,22 @@ mod shredder;
 mod broom;
 #[cfg(feature = "mgc")]
 mod mgc;
+#[cfg(feature = "bacon-rajan-cc")]
 mod bacon_rajan_cc;
 
 pub fn binary_trees_with_parent_pointers(c: &mut Criterion) {
     let group = &mut c.benchmark_group("binary trees with parent pointers");
+    #[cfg(feature = "rc")]
     rc::benchmark_count_binary_trees_with_parent(group);
+    #[cfg(feature = "arc")]
     arc::benchmark_count_binary_trees_with_parent(group);
+    #[cfg(feature = "rust-cc")]
     rust_cc::benchmark_count_binary_trees_with_parent(group);
+    #[cfg(feature = "gc")]
     gc::benchmark_count_binary_trees_with_parent(group);
+    #[cfg(feature = "bacon-rajan-cc")]
     bacon_rajan_cc::benchmark_count_binary_trees_with_parent(group);
+    #[cfg(feature = "safe-gc")]
     safe_gc::benchmark_count_binary_trees_with_parent(group);
     #[cfg(feature = "zb-safe-gc")]
     zb_safe_gc::benchmark_count_binary_trees_with_parent(group);
--- benches/stress_test/mod.rs
+++ benches/stress_test/mod.rs
@@ -2,8 +2,11 @@ use criterion::Criterion;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
+#[cfg(feature = "rust-cc")]
 mod rust_cc;
+#[cfg(feature = "gc")]
 mod gc;
+#[cfg(feature = "safe-gc")]
 mod safe_gc;
 #[cfg(feature = "zb-safe-gc")]
 mod zb_safe_gc;
@@ -13,13 +16,18 @@ mod shredder;
 mod broom;
 #[cfg(feature = "mgc")]
 mod mgc;
+#[cfg(feature = "bacon-rajan-cc")]
 mod bacon_rajan_cc;
 
 pub fn stress_test(c: &mut Criterion) {
     let group = &mut c.benchmark_group("stress test");
+    #[cfg(feature = "rust-cc")]
     rust_cc::benchmark_stress_test(group, &mut create_rng());
+    #[cfg(feature = "gc")]
     gc::benchmark_stress_test(group, &mut create_rng());
+    #[cfg(feature = "bacon-rajan-cc")]
     bacon_rajan_cc::benchmark_stress_test(group, &mut create_rng());
+    #[cfg(feature = "safe-gc")]
     safe_gc::benchmark_stress_test(group, &mut create_rng());
     #[cfg(feature = "zb-safe-gc")]
     zb_safe_gc::benchmark_stress_test(group, &mut create_rng());
